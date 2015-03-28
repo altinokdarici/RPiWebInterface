029882503c86e4ed434e6b76bcb835942b4afccb
Altinok Darici <altinok.darici@outlook.com>
2015-03-28 22:01:27 +0200

    retry login for connection

--- RPi.Client/Program.cs
+++ RPi.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 
 namespace RPi.Client
@@ -6,24 +7,42 @@ namespace RPi.Client
     class Program
     {
         static ManualResetEvent CompletedEvent = new ManualResetEvent(false);
-
+        static bool flagRetry = true;
         public static void Main(string[] args)
         {
             if (WiringPi.Init.WiringPiSetup() != -1)
             {
-                SignalRHelper.Start();
-
-                SignalRHelper.On<Models.Message>("message", OnMessage);
-                CompletedEvent.WaitOne();
+                while (flagRetry)
+                {
+                    try
+                    {
+                        flagRetry = false;
+                        Start();
+                    }
+                    catch (Exception ex)
+                    {
+                        flagRetry = true;
+                    }
+                }
             }
             else
             {
                 Console.WriteLine("WiringPiSetup failed");
             }
+            CompletedEvent.WaitOne();
             Console.ReadLine();
+        }
+        private static void Start()
+        {
+            Console.WriteLine("Starting...");
+
+            SignalRHelper.Start();
+
+            SignalRHelper.On<Models.Message>("message", OnMessage);
+
+
 
         }
-
         private static void OnMessage(Models.Message message)
         {
             Console.WriteLine(string.Format("GPIO_GEN {0}-{1} at {2} ---", message.GpioNo, message.Status, DateTime.Now.ToString()));
@@ -31,7 +50,7 @@ namespace RPi.Client
             {
                 WiringPi.SoftPwm.Create(message.GpioNo, 0, 100);
                 WiringPi.SoftPwm.Write(message.GpioNo, 255);
-             
+
             }
             else
             {
